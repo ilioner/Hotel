810fb91d6da116c1dbdd87c1a4c74785c2d7795f
zhang.qinlong <zhang.qinlong@monstar-lab.com>
2014-03-26 17:35:21 +0800

    modify

--- src/com/hotel/schedule/ScheduleServlet.java
+++ src/com/hotel/schedule/ScheduleServlet.java
@@ -1,6 +1,7 @@
 package com.hotel.schedule;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,6 +33,7 @@ public class ScheduleServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("-----//////---");
 		response.sendRedirect("UI/comment/success.html");
 	}
 
